e57994adeca7039e29f45531c41f64c433af4075
Im-Soumya <aeie2019006@rcciit.org.in>
2023-03-31 21:18:46 +0530

    fixed mobile view of builds page

--- src/pages/builds.tsx
+++ src/pages/builds.tsx
@@ -1,8 +1,8 @@
 import { Box, Flex } from "@chakra-ui/react"
 
-import { DEFAULT_X_PADDING, DEFAULT_FONT_SIZES } from "../lib/constants/ui"
 import Navbar from "../components/Misc/Navbar"
 import Builds from "../components/Sections/Builds"
+import { DEFAULT_X_PADDING, DEFAULT_FONT_SIZES } from "../lib/constants/ui"
 
 const builds = () => {
   return (
@@ -14,19 +14,18 @@ const builds = () => {
       py={{
         base: "10",
         md: "20",
-        // lg: "36"
       }}
       display="flex"
-      alignItems="center" 
+      alignItems={{ md: "center" }} 
       justifyContent="center" 
       color="white"
-      fontSize={DEFAULT_FONT_SIZES}
+      fontSize={ DEFAULT_FONT_SIZES }
       overflowX="hidden"
     >
       <Flex
-        px={DEFAULT_X_PADDING}
-        gap={{base: "8", md:"16"}}
-        direction={{base: "column", md: "row"}}
+        px={ DEFAULT_X_PADDING }
+        gap={{ base: "8", md:"16" }}
+        direction={{ base: "column", md: "row" }}
       >
         <Navbar />
         <Flex 
