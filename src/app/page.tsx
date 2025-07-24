624682da1cc13024d0720a41cdfd6a50db901829
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-07-25 00:15:49 +0530

    add: new setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,8 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1 className='mt-12 mb-6 text-xl font-semibold tracking-tighter'>
-          soumya mukherjee
-        </h1>
-
-        <div>
-          {/* <p className='mb-6'>
-            building solid and scalable products with delightful user experience
-            boosts me up to learn more new technologies.
-          </p> */}
-          <ul>
-            <li>
-              &#x2022; frontend dev{' '}
-              <Link
-                href='https://routerprotocol.com'
-                className='hover:underline underline-offset-2 text-blue-500'
-              >
-                @routerprotocol
-              </Link>
-            </li>
-            <li>&#x2022; exploring fullstack dev & web3.</li>
-            <li>&#x2022; trying to figure out things.</li>
-          </ul>
-        </div>
+    <div className="container mx-auto min-h-screen w-full max-w-[100ch] px-4">
+      <main className="py-20">
+        <h1 className="font-mono">soumya mukherjee</h1>
       </main>
     </div>
   );
