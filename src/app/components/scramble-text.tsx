c29e148e8166405d22fceeb306ce56ec7d8d4beb
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-07-25 09:55:45 +0530

    add: scramble text for headings

---
+++ src/app/components/scramble-text.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { useScramble } from 'use-scramble';
+
+export default function ScrambleText({
+  text,
+  speed = 0.5,
+  tick = 1,
+  step = 1,
+  scramble = 5,
+  seed = 3,
+}: {
+  text: string;
+  speed?: number;
+  tick?: number;
+  step?: number;
+  scramble?: number;
+  seed?: number;
+}) {
+  const { ref } = useScramble({
+    text,
+    speed,
+    step,
+    scramble,
+    seed,
+  });
+  return <span ref={ref} className="font-sans text-teal-500 text-xl" />;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import ScrambleText from './components/scramble-text';
+
 export default function Home() {
   return (
     <div className="container mx-auto min-h-screen w-full max-w-[100ch] px-4">
       <main className="py-20">
-        <h1 className="font-mono">soumya mukherjee</h1>
+        <ScrambleText text="soumya mukherjee" />
       </main>
     </div>
   );
