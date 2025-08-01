7cd7d20c6b6ae67ede8409174f35b2f80604ba6d
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-08-01 13:36:56 +0530

    add: posthog analytics

--- src/components/provider/index.tsx
+++ src/components/provider/index.tsx
@@ -2,15 +2,20 @@
 
 import { ThemeProvider } from "next-themes";
 import type * as React from "react";
+import PosthogPageView from "@/lib/posthog/posthog-page-vew";
+import { CSPostHogProvider } from "@/lib/posthog/posthog-provider";
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <CSPostHogProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        disableTransitionOnChange
+      >
+        {children}
+        <PosthogPageView />
+      </ThemeProvider>
+    </CSPostHogProvider>
   );
 }
