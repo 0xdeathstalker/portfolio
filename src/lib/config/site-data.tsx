fd0de52e59e3bb1f314c3d68c2d73e92549fec37
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-08-07 14:19:36 +0530

    update: social link

--- src/lib/config/site-data.tsx
+++ src/lib/config/site-data.tsx
@@ -4,11 +4,12 @@ import type { Project, Work } from "@/lib/types";
 export const portfolio = {
   author: "soumya mukherjee",
   bio: "i build fast and sharp interfaces. obsessed with clean ui and smooth ux. developing the skill to craft polished web experiences. diving deep in web3. if it's sleek, snappy, and slightly overengineered - chances are i made it.",
-  resume: "https://drive.google.com/file/d/19NML86_Fj4YxKxNGYzT2qynNRJfFKJvv/view?usp=sharing",
+  resume:
+    "https://drive.google.com/file/d/19NML86_Fj4YxKxNGYzT2qynNRJfFKJvv/view?usp=sharing",
   socials: {
     github: "https://github.com/0xdeathstalker",
     linkedin: "https://www.linkedin.com/in/soumya-mukherjee-88a551248/",
-    twitter: "https://x.com/0xmukherjee",
+    twitter: "https://x.com/0xdeathstalker",
   },
   mail: "soumyamukherjee.work@gmail.com",
 } as const;
@@ -70,7 +71,14 @@ export const projects: Array<Project> = [
       "eliminated layout shift and loading jank with full non-blocking data flow",
       "designed keyboard-driven cmdk navigation for rapid search experience",
     ],
-    technologies: ["next.js", "tailwindcss", "shadcnui", "zod", "zustand", "typescript"],
+    technologies: [
+      "next.js",
+      "tailwindcss",
+      "shadcnui",
+      "zod",
+      "zustand",
+      "typescript",
+    ],
     href: "https://nitro-explorer.vercel.app",
   },
   {
@@ -98,7 +106,15 @@ export const projects: Array<Project> = [
       "added system messages for join/leave activity and contextual flow",
       "implemented graceful UX for handling expired or invalid rooms",
     ],
-    technologies: ["next.js", "tailwind css", "convex", "react query", "zod", "uploadthing", "posthog"],
+    technologies: [
+      "next.js",
+      "tailwind css",
+      "convex",
+      "react query",
+      "zod",
+      "uploadthing",
+      "posthog",
+    ],
     href: "https://whispr-room.vercel.app",
   },
 ] as const;
